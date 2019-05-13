82984eea14fe6e7fec18e1623ff054f0837fcd59
Roberto <robertopescee@hotmail.com>
2019-05-13 21:20:30 +0200

    fixed event listener bug

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+var x;
+x = document.querySelector('.submitBtn').addEventListener('click', function(){
+	var test;
+	test = document.querySelector('.newUrl').value;
+	return test;
+	console.log(test*2);
+});
+
+
+
+
+
+
+
+
+
+/*
 //Function constructor
 var Url = function(url){
-	this.url = url
+	this.url = url;
 };
-
-
-
-
-
 
 //Set event listener on click and keypress
 var setupEventListeners = function() {
-	document.getElementById('submitBtn').addEventListener('click', saveItem);
+	document.querySelector('.submitBtn').addEventListener('click', saveItem);
 	document.addEventListener('keypress', function(e){
 		if (e.keyCode === 13 || e.which === 13){
 			saveItem();
@@ -20,22 +32,24 @@ var setupEventListeners = function() {
 
 // Get the input from the input field
 function getInput() {
-	return document.querySelector('.newUrl').value;
+	var newInput = document.querySelector('.newUrl').value;
+	return newInput;
 };
 
 //Create new URL input
-saveItem = function(){
+function saveItem(){
 	var input, newUrl;
 
 	//1. Get the field input data
 	input = getInput();
 	if(input !== "") {
-
 		//2. Create a new URL instance
 		newUrl = new Url(input);
 		return newUrl;
+		console.log(date.url);
+		
 		//Redirect user to the next screen while supplying iframe src with the URL specified by user
-	}
+	};
 };
 
 
@@ -52,3 +66,4 @@ var init = function(){
 
 
 init();
+*/
